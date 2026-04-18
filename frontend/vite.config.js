806178cb95bc4bc328b422f2034100a226164a28
MrDino-Tz <mrdinotz@gmail.com>
2026-04-19 02:46:05 +0300

    fixing some localhost and gh pages issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Pawin-PyPOS/',
-  server: {
-    host: true,
-    allowedHosts: [
-      'bronchially-muggier-clementina.ngrok-free.dev',
-      'localhost',
-      '127.0.0.1'
-    ]
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/Pawin-PyPOS/' : '/',
+    server: {
+      host: true,
+      allowedHosts: [
+        'bronchially-muggier-clementina.ngrok-free.dev',
+        'localhost',
+        '127.0.0.1'
+      ]
+    }
   }
 })
